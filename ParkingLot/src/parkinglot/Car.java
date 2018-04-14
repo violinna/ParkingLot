e72fffb55fe2c46da03a6a02177b6639a0ca88e6
violinna <violinhtg@gmail.com>
2018-04-14 16:59:46 +0700

    method car leave

---
+++ ParkingLot/src/parkinglot/Car.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parkinglot;
+
+/**
+ *
+ * @author violin
+ */
+public class Car {
+    String regNumber, color;
+
+    public Car() {
+    }
+
+    public Car(String regNumber, String color) {
+        this.regNumber = regNumber;
+        this.color = color;
+    }
+
+    public String getRegNumber() {
+        return regNumber;
+    }
+
+    public void setRegNumber(String regNumber) {
+        this.regNumber = regNumber;
+    }
+       
+ 
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+    
+}
--- ParkingLot/src/parkinglot/Parking.java
+++ ParkingLot/src/parkinglot/Parking.java
@@ -6,6 +6,7 @@
 package parkinglot;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -23,15 +24,6 @@ public class Parking {
     Map<String, String> Map2;
     // Map color, List of RegNo
     Map<String, ArrayList<String>> Map3;
-    
-    private class Car {
-        String regNo;
-        String color;
-        public Car(String regNo, String color) {
-            this.regNo = regNo;
-            this.color = color;
-        }
-    }
     
     public void createParkingLot(String count) {
         try {
@@ -53,4 +45,62 @@ public class Parking {
         System.out.println();
     }
     
+    public void tiket(String regNo, String color) {
+        if (this.MAX_SIZE == 0) {
+            System.out.println("Sorry, parking lot is not created");
+            System.out.println();
+        } else if (this.Map1.size() == this.MAX_SIZE) {
+            System.out.println("Sorry, parking lot is full");
+            System.out.println();
+        } else {
+            Collections.sort(slotList);
+            String slot = slotList.get(0).toString();
+            Car car = new Car(regNo, color);
+            this.Map1.put(slot, car);
+            this.Map2.put(regNo, slot);
+            if (this.Map3.containsKey(color)) {
+                ArrayList<String> regNoList = this.Map3.get(color);
+                this.Map3.remove(color);
+                regNoList.add(regNo);
+                this.Map3.put(color, regNoList);
+            } else {
+                ArrayList<String> regNoList = new ArrayList<String>();
+                regNoList.add(regNo);
+                this.Map3.put(color, regNoList);
+            }
+            System.out.println("Allocated slot number: " + slot);
+            System.out.println();
+            slotList.remove(0);
+        }
+    }
+    
+    public void CarLeave(String slotNumber) {
+        if (this.MAX_SIZE == 0) {
+            System.out.println("Sorry, parking lot is not created");
+            System.out.println();
+        } else if (this.Map1.size() > 0) {
+            Car carLeave = this.Map1.get(slotNumber);
+            if (carLeave != null) {
+                this.Map1.remove(slotNumber);
+                this.Map1.remove(carLeave.regNumber);
+                
+                ArrayList<String> regNumberList = this.Map3.get(carLeave.color);
+                
+                if (regNumberList.contains(carLeave.regNumber)) {
+                    regNumberList.remove(carLeave.regNumber);
+                }
+               
+                this.slotList.add(Integer.parseInt(slotNumber));
+                System.out.println("Slot number " + slotNumber + " is free");
+                System.out.println();
+            } else {
+                System.out.println("Slot number " + slotNumber 
+                        + " is already empty");
+                System.out.println();
+            }
+        } else {
+            System.out.println("Parking lot is empty");
+            System.out.println();
+        }
+    }
 }
