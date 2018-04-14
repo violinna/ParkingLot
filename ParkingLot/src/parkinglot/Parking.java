c42d4af534882f8ac20c56b6e27a01f2f84082d9
violinna <violinhtg@gmail.com>
2018-04-14 16:47:26 +0700

    fix parking class

--- ParkingLot/src/parkinglot/Parking.java
+++ ParkingLot/src/parkinglot/Parking.java
@@ -5,12 +5,24 @@
  */
 package parkinglot;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  *
  * @author violin
  */
 public class Parking {
     int MAX_SIZE = 0;
+    
+    ArrayList<Integer> slotList;
+    // Map slot, car
+    Map<String, Car> Map1;
+    // Map regNo, slot
+    Map<String, String> Map2;
+    // Map color, List of RegNo
+    Map<String, ArrayList<String>> Map3;
     
     private class Car {
         String regNo;
@@ -21,5 +33,24 @@ public class Parking {
         }
     }
     
+    public void createParkingLot(String count) {
+        try {
+            this.MAX_SIZE = Integer.parseInt(count);
+        } catch (Exception e) {
+            System.out.println("Invalid lot count");
+            System.out.println();
+        }
+        this.slotList = new ArrayList<Integer>() {};
+        
+        for (int i=1; i<= this.MAX_SIZE; i++) {
+            slotList.add(i);
+        }
+        
+        this.Map1 = new HashMap<String, Car>();
+        this.Map2 = new HashMap<String, String>();
+        this.Map3 = new HashMap<String, ArrayList<String>>();
+        System.out.println("Created parking lot with " + count + " slots");
+        System.out.println();
+    }
     
 }
